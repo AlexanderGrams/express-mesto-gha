1cdfe30fc48970da062ac699e146ca746b70ec12
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-03-17 23:50:14 +0300

    feat: create controllers and create router

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./routes');
 const { PORT = 3000 } = process.env;
-
-const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+})
+  .then(() => console.log("соединение установленно!"))
+  .catch(() => console.log("нет соединение!"))
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Сервер запустился!!! Работает на порту - ${PORT}`)
